2ef5abb2eda5f221663feec8c28cf2014bf214a4
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-02 15:20:51 -0600

    added 2/28 problem

---
+++ 2:26.rb
@@ -0,0 +1,14 @@
+# Write a method called `double_numbers` that takes in an array and returns a new array with each number doubled. Use an `each` loop in the method.
+
+def double_numbers(input_array)
+  doubled_array = []
+
+  input_array.each do |number|
+    doubled_array << number * 2
+  end
+
+  p doubled_array
+
+end
+
+double_numbers([4, 1, 3])  #=> "[8, 2, 6]"
